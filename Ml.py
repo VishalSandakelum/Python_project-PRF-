ee4b7fecc7dfa11a369a652565b9953920f1c06f
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-07-14 15:41:42 +0530

    modify

--- Ml.py
+++ Ml.py
@@ -1,7 +1,7 @@
 import os
 
 NamArray = []
-GradeArray = []
+IDArray = []
 bool(True)
 Iput = ""
 
@@ -10,10 +10,15 @@ def callMethod():
     if Iput.__eq__("1") :
         PrintTerminal()
         AddStudent()
+        
+    elif Iput.__eq__("2"):
+        PrintTerminal()
+        AddMarks()
 
 
 def Printmain() :
     print("1) Add Student"+"\t"+"2) Add Marks");
+    print(IDArray)
     callMethod()
     
 def PrintTerminal():
@@ -28,17 +33,32 @@ def AddStudent():
         else :
             print("Please enter the name !") 
         
-        Grade = input("Enter Grade :")
-        if Grade != "" :
-            GradeArray.append(Grade)
+        id = input("Enter Id :")
+        if id != " " :
+            print("Id"+": "+id)
+            if len(IDArray) == 0:
+                IDArray.append(id)
+            else:
+                for i in range(len(IDArray)):
+                    if id != IDArray[i] :
+                        print(IDArray[i])
+                        IDArray.append(id)
         else :
-            print("Please enter the Grade !")
+            print("Please enter the Id !")
             
         Decition = input("If you wan't to go back enter the 'y'/'n' : ")
         if Decition.__eq__("Y") or Decition.__eq__("y") :
             bool(False)
             PrintTerminal()
             Printmain()
+        
+            
+def AddMarks():
+    nam = input("Enter Student Name : ")
+    if nam !="":
+        print(nam)
+        
+    
 
 class First:
     
